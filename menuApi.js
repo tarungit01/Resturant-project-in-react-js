a5ab8fb3b2a1234395bc0f83766a180a81471b7c
Tarun Singh <67340592+tarungit01@users.noreply.github.com>
2022-01-17 23:01:31 +0530

    Add files via upload

---
+++ menuApi.js
@@ -0,0 +1,125 @@
+const Menu = [
+{
+	id: 1,
+	image: "Images/maggi.png",
+	name: "Maggi",
+	category: "breakfast",
+	price: "25Rs.",
+	description:
+	"This dises is very popular of our resturant and if any one take breakfast once then they will come must be again.",
+
+},
+{
+	id: 2,
+	image: "Images/onionpakora.jpg",
+	name: "Aloopakora",
+	category: "breakfast",
+	price: "25Rs.",
+	description:
+	"This dises is very popular of our resturant and if any one take breakfast once then they will come must be again.",	
+},
+
+{
+	id: 3,
+	image: "Images/samosa.jpg",
+	name: "Samosa",
+	category: "breakfast",
+	price: "25Rs.",
+	description:
+	"This dises is very popular of our resturant and if any one take breakfast once then they will come must be again.",	
+},
+
+{
+	id: 4,
+	image: "Images/paneer.jpg",
+	name: "Matar paneer sabji and rice and roti.",
+	category: "lunch",
+	price: "60Rs.",
+	description:
+	"In our resturant each Sunday made this dises."
+},
+
+{
+	id: 5,
+	image: "Images/dal.png",
+	name: "lady finger sabji, daal, rice and roti.",
+	category: "lunch",
+	price: "60Rs.",
+	description:
+	"This dishes is each day made in our resturant."
+},
+
+{
+	id: 6,
+	image: "Images/chawal.jpg",
+	name: "Lauki ki sabji, daal, rice and roti.",
+	category: "lunch",
+	price: "60Rs.",
+	description:
+	"This dishes is each day made in our resturant."
+},
+
+{
+	id: 7,
+	image: "Images/teacoffee.jpg",
+	name: "Teacoffee",
+	category: "evening",
+	price: "25Rs.",
+	description:
+	"This dishes is each day made in our resturant."
+
+},
+
+{
+	id: 8,
+	image: "Images/teanamkeen.jpg",
+	name: "Teanamkeen.",
+	category: "evening",
+	price: "25Rs.",
+	description:
+	"In our resturant each Sunday made this dises."
+},
+
+{
+	id: 9,
+	image: "Images/teasweat.jpg",
+	name: "Teasweat.",
+	category: "evening",
+	price: "25Rs.",
+	description:
+	"This dishes is each day made in our resturant."
+},
+{
+	id: 10,
+	image: "Images/aloogobhi.jpg",
+	name: "Aloogobhi",
+	category: "dinner",
+	price: "60Rs.",
+	description:
+	"This dises is very popular of our resturant and if any one take breakfast once then they will come must be again.",
+
+},
+
+{
+	id: 11,
+	image: "Images/baingan.jpg",
+	name: "Aloobagan.",
+	category: "dinner",
+	price: "60Rs.",
+	description:
+	"This dises is very popular of our resturant and if any one take breakfast once then they will come must be again.",
+},
+
+{
+	id: 12,
+	image: "Images/masroom.jpg",
+	name: "Mushroom.",
+	category: "dinner",
+	price: "60Rs.",
+	description:
+	"This dises is very popular of our resturant and if any one take breakfast once then they will come must be again.",
+},
+];
+
+
+export default Menu;
