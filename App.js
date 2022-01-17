38b15495eff363725841a09e56cef01bfb57892e
Tarun Singh <67340592+tarungit01@users.noreply.github.com>
2022-01-17 23:04:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Resturant from './Component/Basic/Resturant';
+
+
+const App = () => {
+
+  return(
+  		<Resturant />
+  		
+  	);
+
+};
+
+export default App;
+
